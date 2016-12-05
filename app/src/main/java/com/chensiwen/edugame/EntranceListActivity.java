1e882c5d253b3ca0f16af1da18e87525f8eb0ec9
Chanson <84365252@qq.com>
2016-12-05 17:13:18 +0800

    change: add skeleton using databinding
    Change-Id: Id274c02936900c3eb89904ae7440e75fd766c0bc

---
+++ app/src/main/java/com/chensiwen/edugame/EntranceListActivity.java
@@ -0,0 +1,49 @@
+package com.chensiwen.edugame;
+
+import android.content.Intent;
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+
+import com.chensiwen.edugame.databinding.ActivityEntranceListBinding;
+
+public class EntranceListActivity extends AppCompatActivity {
+
+    public class BindHanlders {
+        public void onClickNumbers(View view) {
+            startActivity(new Intent(EntranceListActivity.this, NumbersActivity.class));
+        }
+
+        public void onClickAVD(View view) {
+            startActivity(new Intent(EntranceListActivity.this, AnimatedVectorDrawableActivity.class));
+        }
+    }
+
+    private ActivityEntranceListBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = DataBindingUtil.inflate(LayoutInflater.from(this), R.layout.activity_entrance_list, null, false);
+        setContentView(binding.getRoot());
+        Toolbar toolbar = binding.toolbar;
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = binding.fab;
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        BindHanlders handlers = new BindHanlders();
+        binding.setHandlers(handlers);
+
+    }
+
+}
