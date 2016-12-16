d980c46bd00ef69756531a53916bf8bd82a59a82
Chanson <84365252@qq.com>
2016-12-16 16:10:30 +0800

    change: add raJava example with exception
    Change-Id: I97cb1eeb801b4cbd9ee37a266a35309d5dcfb16c

--- app/src/main/java/com/chensiwen/edugame/RxJavaExampleActivity.java
+++ app/src/main/java/com/chensiwen/edugame/RxJavaExampleActivity.java
@@ -10,31 +10,94 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ImageView;
 
-import com.chensiwen.edugame.databinding.ActivityAnimatedVectorDrawableBinding;
 import com.chensiwen.edugame.databinding.ActivityRxjavaExampleBinding;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import io.reactivex.Observable;
+import io.reactivex.Observer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
 
 public class RxJavaExampleActivity extends BaseAppCompatActivity {
     private static final String TAG = "RxJavaExampleActivity";
 
+    //private Observable<String> mRepeatedOb = Observable.just("1");
+    //private Observable<String> mRepeatedOb = Observable.just("1","2", "3","4");
+    private Observable<String> mRepeatedOb;
     public class BindHanlders {
         public void onClickNumbers(View view) {
         }
 
         public void onClickTest1(View view) {
             Log.d(TAG, "onClickTest1: ");
+            //mRepeatedOb = Observable.fromCallable(getStringCallable(1));
+            mRepeatedOb = Observable.fromFuture(executor.submit(getStringCallable(1)));
             binding.rxTest1.append("clicked!");
+            binding.rxTest1.append("\n");
+            mRepeatedOb.subscribeOn(Schedulers.newThread())
+                    //.observeOn(AndroidSchedulers.mainThread())
+                    .subscribe(getObserver());
         }
 
-        public void onClickAnimated(View view) {
-            ImageView imageView = binding.animated;
-            Drawable drawable = imageView.getDrawable();
-            if (drawable instanceof Animatable) {
-                ((Animatable) drawable).start();
-            }
+        public void onClickTest2(View view) {
+            Log.d(TAG, "onClickTest2: ");
+            // why this will crash, with java.util.concurrent.ExecutionException: android.view.ViewRootImpl$CalledFromWrongThreadException: Only the original thread that created a view hierarchy can touch its views.
+            mRepeatedOb = Observable.fromFuture(executor.submit(getStringCallable2(2)));
+            binding.rxTest2.append("clicked!");
+            binding.rxTest2.append("\n");
+            mRepeatedOb.subscribeOn(Schedulers.newThread())
+                    //.observeOn(AndroidSchedulers.mainThread())
+                    .subscribe(getObserver());
         }
+
+    }
+
+    private Callable<String> getStringCallable(final int index) {
+        binding.rxTest1.append("getStringCallable!");
+        binding.rxTest1.append("\n");
+        return new Callable<String>() {
+            @Override
+            public String call() throws Exception {
+                binding.rxTest1.append("call!");
+                //try {
+                //    Thread.sleep(500);
+                //}catch (InterruptedException e) {
+                //    e.printStackTrace();
+                //}
+                binding.rxTest1.append("" + Thread.currentThread().getId());
+                binding.rxTest1.append("\n");
+                binding.rxTest1.setText(binding.rxTest1.getText() + "set text \n");
+                return String.valueOf(index);
+            }
+        };
+    }
+
+    private Callable<String> getStringCallable2(final int index) {
+        binding.rxTest2.append("getStringCallable!");
+        binding.rxTest2.append("\n");
+        return new Callable<String>() {
+            @Override
+            public String call() throws Exception {
+                binding.rxTest2.append("call!");
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                binding.rxTest2.append("" + Thread.currentThread().getId());
+                binding.rxTest2.append("\n");
+                binding.rxTest2.setText(binding.rxTest2.getText() + "set text \n");
+                return String.valueOf(index);
+            }
+        };
     }
 
     private ActivityRxjavaExampleBinding binding;
+    private ExecutorService executor = Executors.newSingleThreadExecutor();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,4 +111,37 @@ public class RxJavaExampleActivity extends BaseAppCompatActivity {
 
     }
 
+    private Observer<String> getObserver() {
+        return new Observer<String>() {
+            @Override
+            public void onSubscribe(Disposable d) {
+                binding.rxTest1.append("onSubscribe!");
+                binding.rxTest1.append("" + Thread.currentThread().getId());
+                binding.rxTest1.append("\n");
+            }
+
+            @Override
+            public void onNext(String value) {
+                binding.rxTest1.append("onNext!" + value);
+                binding.rxTest1.append("" + Thread.currentThread().getId());
+                binding.rxTest1.append("\n");
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                e.printStackTrace();
+                binding.rxTest1.append("onError!");
+                binding.rxTest1.append("" + Thread.currentThread().getId());
+                binding.rxTest1.append("\n");
+            }
+
+            @Override
+            public void onComplete() {
+                binding.rxTest1.append("onComplete!");
+                binding.rxTest1.append("" + Thread.currentThread().getId());
+                binding.rxTest1.append("\n");
+
+            }
+        };
+    }
 }
